3cbf1fff485a087e47a5b010e9329e824a93b7bb
naveena-kothamasu <naveenakothamasu@gmail.com>
2024-10-25 12:02:01 +0530

    Create oddoreven.c

---
+++ oddoreven.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int  n;
+    printf("enter n value");
+    scanf("%d",&n);
+    if((n%2)==0)
+    {
+        printf("the given no is even");
+    }
+    else
+    {
+        printf("the given no is odd");
+    }
+}
