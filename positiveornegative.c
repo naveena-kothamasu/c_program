4829fd7c0097fae818b1d2b10d4a132e5c77ad58
naveena-kothamasu <naveenakothamasu@gmail.com>
2024-10-25 11:54:23 +0530

    Create positiveornegative.c

---
+++ positiveornegative.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter n value:");
+    scanf("%d",&n);
+    if(n>=0)
+    {
+        printf("the number is positive");
+    }
+    else
+    {
+        printf("the number is negative");
+    }
+}
