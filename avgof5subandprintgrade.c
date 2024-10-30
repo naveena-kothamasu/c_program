165c568b5e1fa5277172ec036349679e77ef269f
naveena-kothamasu <naveenakothamasu@gmail.com>
2024-10-30 15:48:52 +0530

    Create avgof5subandprintgrade.c

---
+++ avgof5subandprintgrade.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,e;
+    float avg;
+    printf("enter the marks of 5 subjects:");
+    scanf("%d%d%d%d%d",&a,&b.&c,&d,&e);
+    avg=(a+b+c+d+e)/5;
+    printf("the average of 5 subjects:%f",avd);
+    if(avg>90)
+    {
+        printf("A+");
+    }
+        else if(80>avg<90)
+        {
+            printf("A");
+        }
+        else if(70>avg<80)
+        {
+            printf("B");
+        }
+        else 
+    }
+    
+    
+}
