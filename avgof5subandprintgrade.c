c7e6efb6dbef554bcfb7d0dd96cc4e030e969b59
naveena-kothamasu <naveenakothamasu@gmail.com>
2024-11-01 14:50:46 +0530

    Update avgof5subandprintgrade.c

--- avgof5subandprintgrade.c
+++ avgof5subandprintgrade.c
@@ -4,23 +4,32 @@ int main()
     int a,b,c,d,e;
     float avg;
     printf("enter the marks of 5 subjects:");
-    scanf("%d%d%d%d%d",&a,&b.&c,&d,&e);
+    scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
     avg=(a+b+c+d+e)/5;
-    printf("the average of 5 subjects:%f",avd);
+    printf("the average of 5 subjects:%f",avg);
     if(avg>90)
     {
         printf("A+");
     }
-        else if(80>avg<90)
+        else if(avg>=90)
         {
             printf("A");
         }
-        else if(70>avg<80)
+        else if(avg>=80)
         {
             printf("B");
         }
-        else 
-    }
-    
-    
+        else if(avg>=70)
+        {
+            printf("C");
+        }
+        else if(avg>=60)
+        {
+            printf("D");
+        }
+        else
+        {
+            printf("Fail");
+        }
 }
+        
