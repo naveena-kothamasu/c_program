9b6a5ca9546de5a5777abacb9dda32a53bbb06b2
naveena-kothamasu <naveenakothamasu@gmail.com>
2025-01-10 11:11:53 +0530

    Create usingarrayelementscanbereadandprint.c

---
+++ usingarrayelementscanbereadandprint.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a[10],i,n;
+    printf("enter size");
+    scanf("%d",&n);
+    printf("enter the elements");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d\t",a[i]);
+    
+    }
+}
