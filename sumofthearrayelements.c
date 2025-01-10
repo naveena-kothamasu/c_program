ad0b71479b9ac317171d9a6e6f5208104cb8ce02
naveena-kothamasu <naveenakothamasu@gmail.com>
2025-01-10 11:15:01 +0530

    Create sumofthearrayelements.c

---
+++ sumofthearrayelements.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a[10],i,n,sum=0;
+    printf("enter size");
+    scanf("%d",&n);
+    printf("enter the elements");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+       sum=sum+a[i];
+    
+    }
+    printf("the sum of the elements is %d",sum);
+}
